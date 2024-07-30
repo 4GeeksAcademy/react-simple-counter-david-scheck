84ed2371f5ff42d9752d719fc3fcbe8aef48aff1
scheck93 <109469032+scheck93@users.noreply.github.com>
2024-07-30 02:31:02 +0000

    added one bonus item

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,19 @@
 import React from "react";
+import { Counter } from "./Counter";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+	
+	const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      backgroundColor: '#f0f0f0' 
+    }}>
+      <Counter />
+    </div>
+  );
+}
 
 export default Home;
